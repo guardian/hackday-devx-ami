2f3a3767c10943c8f91f13748f4f766ae60e9ff4
Ashleigh Carr <ashcorr20@gmail.com>
2024-06-14 09:46:47 +0100

    Update snapshot

--- lib/devx-container-ami.test.ts
+++ lib/devx-container-ami.test.ts
@@ -1,12 +1,15 @@
-import { App } from "aws-cdk-lib";
-import { Template } from "aws-cdk-lib/assertions";
-import { DevxContainerAmi } from "./devx-container-ami";
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { DevxContainerAmi } from './devx-container-ami';
 
-describe("The DevxContainerAmi stack", () => {
-  it("matches the snapshot", () => {
-    const app = new App();
-    const stack = new DevxContainerAmi(app, "DevxContainerAmi", { stack: "playground", stage: "TEST" });
-    const template = Template.fromStack(stack);
-    expect(template.toJSON()).toMatchSnapshot();
-  });
+describe('The DevxContainerAmi stack', () => {
+	it('matches the snapshot', () => {
+		const app = new App();
+		const stack = new DevxContainerAmi(app, 'DevxContainerAmi', {
+			stack: 'playground',
+			stage: 'TEST',
+		});
+		const template = Template.fromStack(stack);
+		expect(template.toJSON()).toMatchSnapshot();
+	});
 });
